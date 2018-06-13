a37a980ec2ba32a9f1cfadb6f1614671adddccb3
Durant Lin <P19201@yahoo.com.tw>
2018-06-13 14:29:58 +0800

    Init

--- app.py
+++ app.py
@@ -42,6 +42,14 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         message)
+		
+if MessageEvent=="MVP":
+	@handler.add(MessageEvent, message=TextMessage)
+def handle_message_MVP(event):
+    message = TextSendMessage(text="Durant")
+    line_bot_api.reply_message(
+        event.reply_token,
+        message)
 
 import os
 if __name__ == "__main__":
