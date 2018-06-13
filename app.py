d774807e393630cb97050b2c70a09b3620c0f153
Durant Lin <P19201@yahoo.com.tw>
2018-06-13 14:57:15 +0800

    Init

--- app.py
+++ app.py
@@ -38,10 +38,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text="Hello Durant")
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
+	if event.message.text=="MVP":
+		message = TextSendMessage(text="Durant")
+		line_bot_api.reply_message(event.reply_token,message)
+	else:
+		message = TextSendMessage(text="hi")
+		line_bot_api.reply_message(event.reply_token,message)
 		
 import os
 if __name__ == "__main__":
