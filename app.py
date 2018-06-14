dbd7333d9d0cfe71c3e774c5bd0ac52be1ed8d0c
Durant Lin <P19201@yahoo.com.tw>
2018-06-14 11:22:19 +0800

    Init

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def handle_message(event):
 		line_bot_api.reply_message(event.reply_token,message)
 	elif event.message.text=="抽":
 		message = ImageSendMessage(
-		original_content_url='https://i.imgur.com/ebLtiKR.jpg'
-		#preview_image_url='https://i.imgur.com/8sNfqJl.jpg')
+		preview_content_url='https://i.imgur.com/ebLtiKR.jpg',
+		original_image_url='https://i.imgur.com/8sNfqJl.jpg')
 		line_bot_api.reply_message(event.reply_token, message)
 
 
