3576f4072445287f030a4933012cd81f829b9db1
Durant Lin <P19201@yahoo.com.tw>
2018-06-26 14:59:47 +0800

    Init

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def movie():
 	content = ""
 	for index, data in enumerate(soup.select('div.movielist_info h1 a')):
 		if index == 20:
-			return content       
+			return content
+		print(data)
 		title = data.text
 		link = data['href']
 		content+='{}\n{}\n'.format(title,link)
