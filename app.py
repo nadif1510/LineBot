72609ccc908404cbf6fd8452214a3678e052d7b5
Durant Lin <P19201@yahoo.com.tw>
2018-06-29 14:46:56 +0800

    Init

--- app.py
+++ app.py
@@ -95,11 +95,12 @@ def neihu_weather():
 	content = ""
 	title = selector.xpath('//img/@title')
 	link = selector.xpath('//img[@title]/@src')
-	day = selector.xpath('//img[@title]/@src')
+	day = selector.xpath('//td[text()="日期"]/../td[@colspan]//text()')
+	night = selector.xpath('//td[text()="時間"]/../*[text()!="時間"]//text()')
 	for i in range(len(link)):
 		link[i] = 'https://www.cwb.gov.tw'+link[i]
 	for i in range(14):
-		content+='{}\n{}\n'.format(title[i],link[i])
+		content+='{},{}\n'.format(title[i],link[i])
 	return content
 	
 @handler.add(MessageEvent, message=TextMessage)
