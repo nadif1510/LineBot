2ac0922b957501dcda0ecae34ff5360ef1e2c862
durant_lin <P19201@yahoo.com.tw>
2018-07-18 16:38:19 +0800

    Init

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ def handle_message(event):
 			textt=""
 			textt+=event.message.text
 			if textt!="":
-				worksheet.append_row(datetime.datetime.now(), textt)
+				worksheet.append_row(textt)
 				print('新增一列資料到試算表' ,GSpreadSheet)
 				return textt                 
 
